4dbea6d6323b4f722b5db898cd8ede5a990995d3
My name <sindanim@gmail.com>
2022-09-24 14:32:52 +0200

    changed the logo svg

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 //using selectors inside the element
 
-// // traversing the dom
+const questions = 
 
-const btns = document.querySelectorAll(".question-btn")
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement
-    question.classList.toggle("show-text")
-  })
-})
+
+
+
+
+
+
+// // // traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn")
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement
+//     question.classList.toggle("show-text")
+//   })
+// })
