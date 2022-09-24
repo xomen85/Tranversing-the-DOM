6536b603a0063bdac88a5023ed71c5178763b6c0
My name <sindanim@gmail.com>
2022-09-24 14:50:40 +0200

    modified:   app.js 	modified:   logo.svg

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //using selectors inside the element
 
-const questions = 
+const questions = document.querySelectorAll(".question")
 
-
-
-
-
-
-
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn")
+  //console.log(btn)
+  btn.addEventListener("click", function () {
+    question.classList.toggle("show-text")
+  })
+})
 
 // // // traversing the dom
 
