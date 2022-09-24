59bcf955634372e9227b8c92c8b7b686eeb11598
My name <sindanim@gmail.com>
2022-09-24 15:10:13 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const questions = document.querySelectorAll(".question")
 
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn")
-  //console.log(btn)
+  console.log(btn)
   btn.addEventListener("click", function () {
+    // close all the questions when one is open
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text")
+      }
+    })
+
     question.classList.toggle("show-text")
   })
 })
